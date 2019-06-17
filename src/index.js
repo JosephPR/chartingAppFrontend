fe807996dc84ced202660957175e0cdf012492ab
JosephPR <reilly4181@gmail.com>
2019-06-16 21:44:25 -0600

    styling

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 /* quandi api key: xX6McQAvBk9CJ4zh7REH */
 
-// addBtn.addEventListener('click', () => {
-  //   addChart = !addChart
-  //   if (addChart) {
-    //     chartForm.style.display ='block'
-    //   } else {
-      //     chartForm.style.display = 'none'
-      //   }
-      // })
+
 let addChart = false
 const addBtn = document.querySelector('#new-chart-btn')
 const chartForm = document.querySelector('.container')
@@ -17,15 +10,20 @@ const chartCollectionDiv = document.getElementById('chart-collection')
 const parseJSON = response => response.json()
 //new chart submission
 const addChartForm = document.querySelector('.add-chart-form');
-
+const date = moment().format('MMMM Do YYYY, h:mm a')
+const time = document.querySelector('.time').innerHTML = date
 
 function putChartOnPage(charts){
   charts.forEach(function (chart) {
     chartCollectionDiv.innerHTML += `
     <div class="card" data-id=${chart.id}>
-    <h2>${chart.name}</h2>
     <img style="width: 70%" src="${chart.image}" class="chart-avatar">
+    <h1>${chart.name}</h1>
     <h4> RSI: ${chart.rsi}</h4>
+    <h4> Target: ${chart.target}</h4>
+    <h4> Market Cap: ${chart.market}</h4>
+    <h4> concensus: ${chart.rating}</h4>
+
     </div>
     <hr>`
   })
@@ -36,8 +34,8 @@ fetch(chartUrl)
   .then(putChartOnPage)
 
 addChartForm.addEventListener('submit', function (event) {
-  event.preventDefault();
-
+  // event.preventDefault();
+  // changeInputColor(addChartForm, `${chart.rsi}`)
 
   fetch(chartUrl, {
     method: 'POST',
@@ -47,11 +45,47 @@ addChartForm.addEventListener('submit', function (event) {
     body: JSON.stringify({
       "name": `${event.target.name.value}`,
       "image": `${event.target.image.value}`,
-      "rsi": `${event.target.rsi.value}`
+      "rsi": `${event.target.rsi.value}`,
+      "target": `${event.target.target.value}`,
+      "market": `${event.target.market.value}`,
+      "rating": `${event.target.rating.value}`
     })
   })
     .then(parseJSON)
     .then(putChartOnPage)
-  //making the request based on the input
-  //putting the info on the page by creating a new div card and putting that new chart's name, image and rsi there on the page and appending it to chartCollectionDiv
-})
+});
+
+// bideo stuff
+
+var video = document.querySelector('video')
+  , container = document.querySelector('#container');
+
+var setVideoDimensions = function () {
+  // Video's intrinsic dimensions
+  var w = video.videoWidth
+    , h = video.videoHeight;
+
+  var videoRatio = (w / h).toFixed(2);
+
+  var containerStyles = window.getComputedStyle(container)
+    , minW = parseInt( containerStyles.getPropertyValue('width') )
+    , minH = parseInt( containerStyles.getPropertyValue('height') );
+
+  var widthRatio = minW / w
+    , heightRatio = minH / h;
+
+  if (widthRatio > heightRatio) {
+    var newWidth = minW;
+    var newHeight = Math.ceil( newWidth / videoRatio );
+  }
+  else {
+    var newHeight = minH;
+    var newWidth = Math.ceil( newHeight * videoRatio );
+  }
+
+  video.style.width = newWidth + 'px';
+  video.style.height = newHeight + 'px';
+};
+
+video.addEventListener('loadedmetadata', setVideoDimensions, false);
+window.addEventListener('resize', setVideoDimensions, false);
